0940ece10de05462c3b27fc2eca47560cb64a266
wesjen <jenna.westis@gmail.com>
2021-11-22 20:08:15 +0200

    Planning

--- projects/todo_app/todo-app/src/components/tasks/TaskList.js
+++ projects/todo_app/todo-app/src/components/tasks/TaskList.js
@@ -1,5 +1,16 @@
 function TaskList() {
+  const makeRows = () => {
+    let returnableRows;
+    // TODO: For loop for getting all the task objects
+    return returnableRows;
+  };
+
   // LISTS ALL TASKS
+  return (
+    <table>
+      <tbody>{makeRows}</tbody>
+    </table>
+  );
 }
 
 export default TaskList;
