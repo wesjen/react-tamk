3f06a978d7de57aacd7cc69e5e1a655a5a66bcbd
wesjen <jenna.westis@gmail.com>
2021-11-22 17:45:04 +0200

    Plan: App planning

--- projects/todo_app/todo-app/src/App.js
+++ projects/todo_app/todo-app/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+/* CRITERIA
+
+1. SPA - Single Page Application
+
+2. ToDo - App
+
+3. KOMMENTOI
+
+--------- TARVITAAN --------------
+
+NÄKYMÄT :
+  - ETUSIVU / (TEHTÄVÄLISTAUS)
+  - ???
+  - INFO-NÄKYMÄ
+
+ELEMENTIT :
+  -VALIKKO
+
+  - NÄKYMÄT :
+    - ETUSIVU-ELEMENTTI
+    - INFO-ELEMENTTI
+
+  - TEHTÄVÄT:
+    - TEHTÄVÄ - OTSIKKO
+    - TEHTÄVÄ - KUVAUS
+    - TEHTÄVÄ - PÄIVÄYS
+    - CONTAINER - KAIKKI TEHTÄVÄT
+    - CONTAINER - YKSITTÄINEN TEHTÄVÄ
+    - NAPPI - TEHTÄVIEN LISÄYS
+    - NAPPI - TEHTÄVIEN POISTO
+
+FUNKTIOT :
+  - VALIKKO :
+    - NAVIGOINTI NÄKYMIEN VÄLILLÄ
+
+  - TEHTÄVÄT :
+    - TEHTÄVIEN LISÄYS
+    - TEHTÄVIEN POISTO
+    - TEHTÄVIEN MUOKKAAMINEN
+    - TEHTÄVIEN UUDELLEENJÄRJESTYS LISTALLA (EI SORT?)
+
+
+
+DOKUMENTAATIO :
+  - doc.pdf
+
+1 TASO ----------------
+- 3 ERILAISTA NÄKYMÄÄ
+- doc.pdf TÄYTETTY JA PALAUTETTU
+
+2 TASO ---------------
+- INFO-NÄKYMÄ - TEKIJÄN NIMI
+- INFO NÄKYMÄ - KÄYTTÖOHJEET
+- INFO NÄKYMÄ - LISENSSIT
+
+3 TASO ---------------
+
+
+
+
+*/
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
