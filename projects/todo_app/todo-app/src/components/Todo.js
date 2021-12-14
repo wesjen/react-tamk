b5f96ec1049ad1b5d0a9ef48a46a3bc9150bb4b9
wesjen <jenna.westis@gmail.com>
2021-12-14 15:24:38 +0200

    Interface changes & styling

--- projects/todo_app/todo-app/src/components/Todo.js
+++ projects/todo_app/todo-app/src/components/Todo.js
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
 import Home from "./interfaces/Home";
+import Info from "./interfaces/Info";
 import "./Todo.css";
 
 function ToDo() {
+  const [activeInterface, setActiveInterface] = useState();
+
   // TASKS
   // NÄKYMÄVALITSIN
   // VALIKKO
-  return <Home />;
+
+  function changeInterface(clickedIF) {
+    setActiveInterface(clickedIF);
+  }
+
+  return (
+    <div>
+      <div class="menu-bar">
+        <button onClick={() => changeInterface(<Home />)}>Home</button>
+        <button onClick={() => changeInterface(<Info />)}>Info</button>
+      </div>
+      {activeInterface}
+    </div>
+  );
 }
 
 export default ToDo;
---
+++ projects/todo_app/todo-app/src/components/interfaces/Info.js
@@ -0,0 +1,5 @@
+function Info() {
+  return <p>This is info page</p>;
+}
+
+export default Info;
