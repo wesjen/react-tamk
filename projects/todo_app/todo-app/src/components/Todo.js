1e069c1981d1dc9af9e6ac51ab5d4f166744257f
wesjen <jenna.westis@gmail.com>
2021-12-15 17:07:42 +0200

    Add 'Add' -interface

--- projects/todo_app/todo-app/src/components/Todo.js
+++ projects/todo_app/todo-app/src/components/Todo.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Home from "./interfaces/Home";
 import Info from "./interfaces/Info";
+import AddTask from "./interfaces/AddTask";
 import "./Todo.css";
 
 function ToDo() {
@@ -19,6 +20,8 @@ function ToDo() {
       <div className="menu-bar">
         <button onClick={() => changeInterface(<Home />)}>Home</button>
         <button onClick={() => changeInterface(<Info />)}>Info</button>
+        <button onClick={() => changeInterface(<AddTask />)}>Add +</button>
+        {/* TODO: ADD ICON FOR ADD ? */}
       </div>
       {activeInterface}
     </div>
---
+++ projects/todo_app/todo-app/src/components/interfaces/AddTask.js
@@ -0,0 +1,5 @@
+function AddTask() {
+  return <p>You can add new tasks in this</p>;
+}
+
+export default AddTask;
