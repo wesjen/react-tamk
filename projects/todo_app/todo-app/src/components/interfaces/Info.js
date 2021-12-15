33d0dca514fafbe3e537305c57fbe614fb4e6560
wesjen <jenna.westis@gmail.com>
2021-12-15 17:27:14 +0200

    Info-page

--- projects/todo_app/todo-app/src/components/interfaces/Info.js
+++ projects/todo_app/todo-app/src/components/interfaces/Info.js
@@ -1,5 +1,40 @@
+// TODO: MANUAL/INSTRUCTIONS HOW TO USE
+
 function Info() {
-  return <p>This is info page</p>;
+  return (
+    <div className="info-div">
+      <h1 style={{ color: "red" }}>How to use</h1>
+      <p style={{ color: "red" }}>ADD HOW TO USE INSTRUCTIONS</p>
+      <h2>Creator</h2>
+      <p>Creator of this App is Jenna Westerlund</p>
+      <h2>Licences</h2>
+      <p>Uses Ant Design Icons from react-icons </p>
+      <a href="https://react-icons.github.io/react-icons/icons?name=ai">
+        Link to Ant Design Icons from react-icons
+      </a>
+      <p>Under Open source MIT-license </p>
+      <a href="https://opensource.org/licenses/MIT">
+        Link to Open source MIT-license
+      </a>
+    </div>
+  );
 }
+
+/* Copyright 2021 Jenna Westerlund
+
+Permission is hereby granted, free of charge,
+to any person obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 export default Info;
