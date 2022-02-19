387cda3399e9203c7db22864001262d0ff3af717
wesjen <jenna.westis@gmail.com>
2022-02-20 00:48:38 +0200

    New build & Change URLS

--- src/components/tasks/AddTask.js
+++ src/components/tasks/AddTask.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 function AddTask(props) {
-  const tasksURL = "https://react-todo-app-tamk.herokuapp.com/tasks";
+  const tasksURL = "https://react-todo-app-tamk.herokuapp.com/api/tasks";
   const [todos, setTodos] = useState([]);
   const [newTask, setNewTask] = useState({
     title: "",
